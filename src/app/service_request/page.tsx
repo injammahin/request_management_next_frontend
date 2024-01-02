763e8791ffc635564ac4426d620fdfc58310339b
injammahin <injammahin5507@gmail.com>
2024-01-02 21:19:19 +0600

    up

--- src/app/service_request/page.tsx
+++ src/app/service_request/page.tsx
@@ -29,162 +29,164 @@ const ServiceRequestForm: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto mt-8">
-      <h1 className="text-3xl font-semibold mb-6">Service Request Form</h1>
-      <form className="w-full max-w-lg" onSubmit={handleSubmit}>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="requestNo"
-            >
-              Request No:
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="requestNo"
-              type="text"
-              name="requestNo"
-              value={formData.requestNo}
-              onChange={handleChange}
-            />
-          </div>
+    <div className="flex items-center justify-center h-screen">
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-semibold mb-6">Service Request Form</h1>
+        <form className="w-full max-w-lg" onSubmit={handleSubmit}>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="requestNo"
+              >
+                Request No:
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="requestNo"
+                type="text"
+                name="requestNo"
+                value={formData.requestNo}
+                onChange={handleChange}
+              />
+            </div>
 
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="date"
-            >
-              Date
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="date"
-              type="date"
-              name="date"
-              value={formData.date}
-              onChange={handleChange}
-            />
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="date"
+              >
+                Date
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="date"
+                type="date"
+                name="date"
+                value={formData.date}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="requestedBy"
+              >
+                Requested By:
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="requestedBy"
+                type="text"
+                name="requestedBy"
+                value={formData.requestedBy}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="requestFor"
+              >
+                Request For:
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="requestFor"
+                type="text"
+                name="requestFor"
+                value={formData.requestFor}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="department"
+              >
+                department:
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="department"
+                type="text"
+                name="department"
+                value={formData.department}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="employeeId"
+              >
+                employee Id:
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="employeeId"
+                type="text"
+                name="employeeId"
+                value={formData.employeeId}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-full  px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="designation"
+              >
+                designation:
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="designation"
+                type="text"
+                name="designation"
+                value={formData.designation}
+                onChange={handleChange}
+              />
+            </div>
           </div>
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="requestedBy"
-            >
-              Requested By:
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="requestedBy"
-              type="text"
-              name="requestedBy"
-              value={formData.requestedBy}
-              onChange={handleChange}
-            />
+          {/* Add other form fields with similar structure */}
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="reasonOfRequest"
+              >
+                Reason of Request:
+              </label>
+              <textarea
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="reasonOfRequest"
+                name="reasonOfRequest"
+                value={formData.reasonOfRequest}
+                onChange={handleChange}
+              ></textarea>
+            </div>
           </div>
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="requestFor"
-            >
-              Request For:
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="requestFor"
-              type="text"
-              name="requestFor"
-              value={formData.requestFor}
-              onChange={handleChange}
-            />
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="serviceDetails"
+              >
+                details of service
+              </label>
+              <textarea
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="serviceDetails"
+                name="serviceDetails"
+                value={formData.serviceDetails}
+                onChange={handleChange}
+              ></textarea>
+            </div>
           </div>
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="department"
-            >
-              department:
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="department"
-              type="text"
-              name="department"
-              value={formData.department}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="employeeId"
-            >
-              employee Id:
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="employeeId"
-              type="text"
-              name="employeeId"
-              value={formData.employeeId}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="w-full  px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="designation"
-            >
-              designation:
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="designation"
-              type="text"
-              name="designation"
-              value={formData.designation}
-              onChange={handleChange}
-            />
-          </div>
-        </div>
-        {/* Add other form fields with similar structure */}
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="reasonOfRequest"
-            >
-              Reason of Request:
-            </label>
-            <textarea
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="reasonOfRequest"
-              name="reasonOfRequest"
-              value={formData.reasonOfRequest}
-              onChange={handleChange}
-            ></textarea>
-          </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="serviceDetails"
-            >
-              details of service
-            </label>
-            <textarea
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="serviceDetails"
-              name="serviceDetails"
-              value={formData.serviceDetails}
-              onChange={handleChange}
-            ></textarea>
-          </div>
-        </div>
-        {/* ... Add more form fields as needed ... */}
-        <button type="submit">Submit</button>
-      </form>
+          {/* ... Add more form fields as needed ... */}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
