2bf987522953f7457308523eeb57e80c480cbb35
injammahin <injammahin5507@gmail.com>
2024-01-03 22:32:47 +0600

    c

--- src/app/user/service_request/page.tsx
+++ src/app/user/service_request/page.tsx
@@ -75,6 +75,9 @@ const RequestServiceForm: React.FC<RequestServiceFormProps> = ({}) => {
       setSuccessMessage("Form submitted successfully");
       setErrorMessage("");
       console.log("Form submitted successfully:", response.data);
+      setTimeout(() => {
+        window.location.reload();
+      }, 3000);
     } catch (error) {
       setErrorMessage("Form submission failed");
       setSuccessMessage("");
@@ -83,17 +86,24 @@ const RequestServiceForm: React.FC<RequestServiceFormProps> = ({}) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="max-w-md mx-auto  mt-8 p-4 border">
-      <label className="block  w-1/2 mb-2">
-        requestNo:
+    <form
+      onSubmit={handleSubmit}
+      className="max-w-md mx-auto w-1/2 mt-8 p-4 border"
+    >
+      <div className="relative z-0 w-full mb-5 group">
         <input
           type="text"
           name="requestNo"
+          id="requestNo"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
           onChange={handleInputChange}
-          className="block w-full p-2 border border-gray-300 rounded"
         />
-      </label>
-      <label className="block w-1/2 mb-2">
+        <label className=" uppercase peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+          request no
+        </label>
+      </div>
+      <label className="block mb-2">
         date:
         <input
           type="date"
