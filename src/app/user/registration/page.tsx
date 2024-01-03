fca22c1d6f518fed57ce761f5c65d7b70d74d215
injammahin <injammahin5507@gmail.com>
2024-01-03 20:32:28 +0600

    s

--- src/app/user/registration/page.tsx
+++ src/app/user/registration/page.tsx
@@ -33,7 +33,7 @@ const SignupPage = () => {
       setSuccessMessage("");
     }
   };
-
+  23;
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
