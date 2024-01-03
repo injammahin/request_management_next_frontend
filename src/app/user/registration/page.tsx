57e634c8969e5f53bda262a532267a1ab7d092bd
injammahin <injammahin5507@gmail.com>
2024-01-03 19:13:30 +0600

    registration

---
+++ src/app/user/registration/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+import { useState } from "react";
+import axios from "axios";
+
+const SignupPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post("http://localhost:3001/auth/signup", {
+        name,
+
+        email,
+        password,
+      });
+      console.log(response.data);
+      setSuccessMessage("Signup successful");
+      setErrorMessage("");
+    } catch (error) {
+      console.error("Error signing up:", error);
+      setErrorMessage("Failed to signup. Please try again.");
+      setSuccessMessage("");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign up
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={(e) => e.preventDefault()}>
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="name" className="sr-only">
+                Name
+              </label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                autoComplete="name"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder=" enter Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="email" className="sr-only">
+                email
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder=" enter email adress"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder=" enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="button"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={handleSubmit}
+            >
+              Sign up
+            </button>
+          </div>
+        </form>
+        {successMessage && (
+          <p className="text-green-500 mt-2">{successMessage}</p>
+        )}
+        {errorMessage && <p className="text-red-500 mt-2">{errorMessage}</p>}
+        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+          Already have an account ?
+          <a
+            href="./login"
+            className="text-blue-600 hover:underline dark:text-blue-500"
+          >
+            Sign in
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignupPage;
