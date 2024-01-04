43166809b6ecb890f07d82932f24871127c0441b
injammahin <injammahin5507@gmail.com>
2024-01-04 13:33:53 +0600

    side ber done

--- src/app/components/navigation/sidebar/page.tsx
+++ src/app/components/navigation/sidebar/page.tsx
@@ -1,47 +1,55 @@
 import React from "react";
 import Link from "next/link";
+import {
+  ClipboardCheckIcon,
+  UserIcon,
+  InformationCircleIcon,
+  UsersIcon,
+  KeyIcon,
+} from "@heroicons/react/outline"; // Use outline icons from v2
 
 const Sidebar = () => {
   return (
-    <aside className="bg-gray-300 text-white w-72 min-h-screen">
-      <div className="p-4"></div>
+    <aside className="bg-gray-800 text-white w-64 min-h-screen">
+      <div className="p-4 flex items-center justify-center">
+        <span className="text-2xl font-bold"></span>
+      </div>
       <nav className="mt-4">
         <Link
           href="/user/service_request"
-          className="block py-2 px-4 uppercase text-black hover:text-white text- hover:bg-gray-700"
+          className=" py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
         >
-          Service request
+          <ClipboardCheckIcon className="w-5 h-5" />
+          <span>Service Request</span>
         </Link>
         <Link
           href="/user/profile"
-          className="block py-2 px-4 uppercase text-black hover:text-white hover:bg-gray-700"
+          className=" py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
         >
-          view requested forms
+          <UserIcon className="w-5 h-5" />
+          <span>View Requested Forms</span>
         </Link>
         <Link
           href="/department/department_information"
-          className="block py-2 px-4 uppercase text-black hover:text-white hover:bg-gray-700"
+          className=" py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
         >
-          department Information
+          <InformationCircleIcon className="w-5 h-5" />
+          <span>Department Information</span>
         </Link>
         <Link
           href="/department/employee_information"
-          className="block py-2 px-4 uppercase text-black hover:text-white hover:bg-gray-700"
+          className=" py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
         >
-          employee information
+          <UsersIcon className="w-5 h-5" />
+          <span>Employee Information</span>
         </Link>
         <Link
           href="/user/changepassword"
-          className="block py-2 px-4 uppercase text-black hover:text-white hover:bg-gray-700"
+          className=" py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
         >
-          change password
+          <KeyIcon className="w-5 h-5" />
+          <span>Change Password</span>
         </Link>
-        {/* <Link
-          href="/user/logout"
-          className="block py-2 px-4 uppercase text-black hover:text-white hover:bg-gray-700"
-        >
-          logout
-        </Link> */}
       </nav>
     </aside>
   );
