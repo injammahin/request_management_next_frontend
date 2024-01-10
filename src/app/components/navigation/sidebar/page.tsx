4e68c93de8a420a16442e241b75978f920196bf8
injammahin <injammahin5507@gmail.com>
2024-01-10 11:09:21 +0600

    button

--- src/app/components/navigation/sidebar/page.tsx
+++ src/app/components/navigation/sidebar/page.tsx
@@ -46,49 +46,63 @@ const Sidebar = () => {
           <nav className="mt-4">
             <Link
               href="/user/service_request"
-              className="py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
+              className="text-sm py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
             >
               <ClipboardCheckIcon className="w-5 h-5" />
               <span>Service Request</span>
             </Link>
             <Link
               href="/user/profile"
-              className="py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
+              className="text-sm py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-[#6DA4AA]"
             >
               <UserIcon className="w-5 h-5" />
               <span>View Requested Forms</span>
             </Link>
             <Link
               href="/user/maintenance-request-form"
-              className="py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
+              className=" text-sm py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
             >
               <ClipboardCheckIcon className="w-5 h-5" />
               <span> Create Maintenance Request Form</span>
             </Link>
             <Link
               href="/user/view_maintenance_request_form"
-              className="py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
+              className="text-sm py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-[#6DA4AA]"
             >
               <ClipboardCheckIcon className="w-5 h-5" />
               <span>View Maintenance Request Form</span>
             </Link>
             <Link
               href="/department/department_information"
-              className="py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
+              className="text-sm py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
             >
               <InformationCircleIcon className="w-5 h-5" />
               <span>Department Information</span>
             </Link>
             <Link
+              href="/user/department_information"
+              className="text-sm py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-[#6DA4AA]"
+            >
+              <ClipboardCheckIcon className="w-5 h-5" />
+              <span> View Department Information</span>
+            </Link>
+            <Link
               href="/department/employee_information"
-              className="py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
+              className="text-sm py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-gray-700"
             >
               <UsersIcon className="w-5 h-5" />
               <span>Employee Information</span>
             </Link>
             <Link
+              href="/user/employee_information"
+              className="text-sm py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-[#6DA4AA]"
+            >
+              <ClipboardCheckIcon className="w-5 h-5" />
+              <span>View Employee Information</span>
+            </Link>
+            <Link
               href="/user/changepassword"
-              className="py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-[#5F0F40]"
+              className="text-sm py-2 px-4 flex items-center space-x-2 uppercase text-white hover:bg-[#5F0F40]"
             >
               <KeyIcon className="w-5 h-5" />
               <span>Change Password</span>
---
+++ src/app/user/(view)/department_information/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>view department information</div>;
+};
+
+export default page;
