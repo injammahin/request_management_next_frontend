ff4a88f60faca65ebfbba524755ce3597f1cba54
injammahin <injammahin5507@gmail.com>
2024-01-11 19:49:04 +0600

    profile aahow done

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -4,10 +4,10 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Navbar from "@/app/components/navigation/page";
-import Link from "next/link";
 import LoadingSpinner from "@/app/components/loading/page";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
+import React from "react";
 
 interface UserDetails {
   id: number | undefined;
@@ -56,7 +56,6 @@ const ProfilePage: React.FC = () => {
 
         const fetchedUserData = response.data as UserWithServiceRequests;
 
-        // Initialize showFullForm property for each service request
         fetchedUserData.serviceRequests?.forEach((request) => {
           request.showFullForm = false;
         });
@@ -72,15 +71,12 @@ const ProfilePage: React.FC = () => {
   }, []);
 
   const toggleShowFullForm = (request: ServiceRequest) => {
-    // Toggle the showFullForm property for the clicked request
-    request.showFullForm = !request.showFullForm;
     setUserData((prevUserData) => {
       if (prevUserData) {
-        // If prevUserData is not null, spread its properties and update serviceRequests
-        return {
-          ...prevUserData,
-          serviceRequests: [...prevUserData.serviceRequests!], // Use the non-null assertion operator here
-        };
+        const updatedRequests = prevUserData.serviceRequests?.map((r) =>
+          r.id === request.id ? { ...r, showFullForm: !r.showFullForm } : r
+        );
+        return { ...prevUserData, serviceRequests: updatedRequests };
       }
       return null;
     });
@@ -104,7 +100,6 @@ const ProfilePage: React.FC = () => {
       await axios.delete(
         `http://localhost:3001/service-requests/${selectedRequestId}`
       );
-      // Update state after successful deletion
       setUserData((prevUserData) => {
         if (prevUserData) {
           return {
@@ -128,7 +123,7 @@ const ProfilePage: React.FC = () => {
   };
 
   const handleDownloadPDF = (id: number) => {
-    const doc = new jsPDF() as any; // Type assertion to bypass TypeScript error
+    const doc = new jsPDF() as any;
 
     if (userData) {
       const selectedRequest = userData.serviceRequests?.find(
@@ -172,8 +167,7 @@ const ProfilePage: React.FC = () => {
           </div>
           <div className="space-y-4">
             <p className="text-sm text-gray-700">
-              <span className="font-bold"> your user ID is :</span>{" "}
-              {userData.id}
+              <span className="font-bold">Your user ID is:</span> {userData.id}
             </p>
             <p className="text-sm text-gray-700">
               <span className="font-bold">Name:</span> {userData.name}
@@ -185,7 +179,7 @@ const ProfilePage: React.FC = () => {
               userData.serviceRequests.length > 0 && (
                 <div className="bg-gray-200 p-6 rounded-lg">
                   <h1 className="text-lg font-semibold mb-2">
-                    search your requested form
+                    Search your requested form
                   </h1>
                   <input
                     type="text"
@@ -196,88 +190,105 @@ const ProfilePage: React.FC = () => {
                   />
                   {filteredServiceRequests &&
                   filteredServiceRequests.length > 0 ? (
-                    filteredServiceRequests.map((request) => (
-                      <div
-                        key={request.id}
-                        className="mb-4 bg-white border-b-2 border-gray-300 p-4 relative"
-                      >
-                        <p className="text-sm text-gray-900">
-                          <div className="font-bold">Request No:</div>{" "}
-                          {request.requestNo}{" "}
-                        </p>
-                        {request.showFullForm && (
-                          <form>
-                            <div>
-                              <ul className="list-none">
-                                <li className="border-b-2 border-gray-300 p-4">
-                                  {/* Include the other details here */}
-                                  <p className="text-sm text-gray-900">
-                                    <span className="font-bold">Date:</span>{" "}
-                                    {request.date} |{" "}
-                                    <span className="font-bold">
-                                      Department:
-                                    </span>{" "}
-                                    {request.department} |{" "}
-                                    <span className="font-bold">
-                                      Designation:
-                                    </span>{" "}
-                                    {request.designation} |{" "}
-                                    <span className="font-bold">
-                                      Employee Id:
-                                    </span>{" "}
-                                    {request.employeeId} |{" "}
-                                    <span className="font-bold">
-                                      Reason of Request:
-                                    </span>{" "}
-                                    {request.reasonOfRequest} |{" "}
-                                    <span className="font-bold">
-                                      Request For:
-                                    </span>{" "}
-                                    {request.requestFor} |{" "}
-                                    <span className="font-bold">
-                                      Requested By:
-                                    </span>{" "}
-                                    {request.requestedBy} |{" "}
-                                    <span className="font-bold">
-                                      Service Details:
-                                    </span>{" "}
-                                    {request.serviceDetails}
-                                  </p>
-                                </li>
-                              </ul>
-                            </div>
-                          </form>
-                        )}
-                        <div className="flex space-x-2">
-                          <button
-                            onClick={() => toggleShowFullForm(request)}
-                            className="bg-[#43766C] rounded-lg h-8 w-28 hover:underline text-white"
-                          >
-                            {request.showFullForm ? "Show Less" : "Show More"}
-                          </button>
-                          <button
-                            onClick={() => handleDelete(request.id)}
-                            className="bg-[#9A031E] rounded-lg h-8 w-28 hover:underline text-white"
-                          >
-                            Delete
-                          </button>
-                          <button className="bg-[#4CB9E7] rounded-lg h-8 w-28">
-                            <a href="/user/updatedata">
-                              <a className=" hover:underline text-white">
-                                Edit Profile
-                              </a>
-                            </a>
-                          </button>
-                          <button
-                            type="button"
-                            onClick={() => handleDownloadPDF(request.id)}
-                            className="bg-green-500 text-white h-8 w-32 rounded hover:bg-green-700"
-                          >
-                            Download PDF
-                          </button>
-                        </div>
-                      </div>
-                    ))
+                    <table className="w-full">
+                      <thead>
+                        <tr>
+                          <th className="border p-2">Request No</th>
+                          <th className="border p-2">Show More</th>
+                          <th className="border p-2">Delete</th>
+                          <th className="border p-2">Edit</th>
+                          <th className="border p-2">Download PDF</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {filteredServiceRequests.map((request) => (
+                          <React.Fragment key={request.id}>
+                            <tr>
+                              <td className="border p-2">
+                                {request.requestNo}
+                              </td>
+                              <td className="border p-2">
+                                <button
+                                  onClick={() => toggleShowFullForm(request)}
+                                  className="bg-[#43766C] rounded-lg h-8 w-28 hover:underline text-white"
+                                >
+                                  {request.showFullForm
+                                    ? "Show Less"
+                                    : "Show More"}
+                                </button>
+                              </td>
+                              <td className="border p-2">
+                                <button
+                                  onClick={() => handleDelete(request.id)}
+                                  className="bg-[#9A031E] rounded-lg h-8 w-28 hover:underline text-white"
+                                >
+                                  Delete
+                                </button>
+                              </td>
+                              <td className="border p-2">
+                                <button className="bg-[#4CB9E7] rounded-lg h-8 w-28">
+                                  <a
+                                    href="/user/updatedata"
+                                    className="hover:underline text-white"
+                                  >
+                                    Edit Profile
+                                  </a>
+                                </button>
+                              </td>
+                              <td className="border p-2">
+                                <button
+                                  type="button"
+                                  onClick={() => handleDownloadPDF(request.id)}
+                                  className="bg-green-500 text-white h-8 w-32 rounded hover:bg-green-700"
+                                >
+                                  Download PDF
+                                </button>
+                              </td>
+                            </tr>
+                            {request.showFullForm && (
+                              <tr>
+                                <td colSpan={5} className="p-2">
+                                  <div className="bg-blue-100 p-4 rounded-lg">
+                                    <p className="text-sm text-gray-900">
+                                      <span className="font-bold">Date:</span>{" "}
+                                      {request.date} |{" "}
+                                      <span className="font-bold">
+                                        Department:
+                                      </span>{" "}
+                                      {request.department} |{" "}
+                                      <span className="font-bold">
+                                        Designation:
+                                      </span>{" "}
+                                      {request.designation} |{" "}
+                                      <span className="font-bold">
+                                        Employee Id:
+                                      </span>{" "}
+                                      {request.employeeId} |{" "}
+                                      <span className="font-bold">
+                                        Reason of Request:
+                                      </span>{" "}
+                                      {request.reasonOfRequest} |{" "}
+                                      <span className="font-bold">
+                                        Request For:
+                                      </span>{" "}
+                                      {request.requestFor} |{" "}
+                                      <span className="font-bold">
+                                        Requested By:
+                                      </span>{" "}
+                                      {request.requestedBy} |{" "}
+                                      <span className="font-bold">
+                                        Service Details:
+                                      </span>{" "}
+                                      {request.serviceDetails}
+                                    </p>
+                                  </div>
+                                </td>
+                              </tr>
+                            )}
+                          </React.Fragment>
+                        ))}
+                      </tbody>
+                    </table>
                   ) : (
                     <p className="text-sm text-gray-700">
                       No matching service requests found.
