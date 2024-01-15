019c88d7b85744250c4de3ee431f37c471bf77d4
injammahin <injammahin5507@gmail.com>
2024-01-15 21:23:05 +0600

    w

---
+++ src/app/user/[updatedata]/page.tsx
@@ -0,0 +1,199 @@
+// Import the necessary dependencies
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Link from "next/link";
+export interface ServiceRequest {
+  id: number;
+  requestNo: string;
+  date: string;
+  department: string;
+  designation: string;
+  employeeId: string;
+  reasonOfRequest: string;
+  requestFor: string;
+  requestedBy: string;
+  serviceDetails: string;
+  // Add other fields as needed
+}
+
+interface EditServiceRequestProps {}
+
+const EditServiceRequest: React.FC<EditServiceRequestProps> = () => {
+  const [editFormData, setEditFormData] = useState<ServiceRequest | null>(null);
+  const [requestId, setRequestId] = useState<number | null>(null);
+  console.log(requestId);
+  // Use useEffect to parse query parameters
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const idParam = urlParams.get("requestId");
+
+    setRequestId(Number(idParam));
+  }, []);
+
+  // Use useEffect to fetch data when requestId changes
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.put<ServiceRequest>(
+          `http://localhost:3001/service-requests/${requestId}`
+        );
+        setEditFormData(response.data);
+        console.log("====================================");
+        console.log(response.data);
+        console.log("====================================");
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+
+    // Fetch data only if requestId is available
+    if (requestId !== null) {
+      fetchData();
+    }
+  }, [requestId]);
+
+  const handleUpdateServiceRequest = () => {
+    // Implement your update logic here using editFormData
+    // ...
+  };
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setEditFormData({
+      ...editFormData!,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  return (
+    <form
+      // onSubmit={handleSubmit}
+      className="max-w-3xl uppercase mx-auto mt-8 p-4 border"
+    >
+      <div className="grid  grid-cols-2 gap-4 mb-4">
+        <div className="mb-2 flex flex-row items-center">
+          <label className="flex flex-none mr-2">request no:</label>
+          <input
+            type="text"
+            name="requestNo"
+            id="requestNo"
+            // value={editFormData.requestNo} // Display requestNo
+            readOnly
+            className="block w-full py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          />
+        </div>
+
+        <div className="mb-2 flex flex-row items-center">
+          <label className="flex flex-none mr-2">date:</label>
+          <input
+            type="date"
+            name="date"
+            onChange={handleInputChange}
+            // value={editFormData.date}
+            className=" w-full py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <div className="mb-2 flex flex-row items-center">
+          <label className=" flex flex-none mr-2">request by : </label>
+          <input
+            type="text"
+            name="requestedBy"
+            onChange={handleInputChange}
+            // value={serviceDetails.requestedBy}
+            className="block w-full  py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          />
+        </div>
+
+        <div className="mb-2 flex flex-row items-center">
+          <label className="flex flex-none mr-2">request For:</label>
+          <input
+            type="text"
+            name="requestFor"
+            onChange={handleInputChange}
+            // value={serviceDetails.requestFor}
+            className="block w-full py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <div className="mb-2 flex flex-row items-center">
+          <label className="flex flex-none mr-2">department:</label>
+          <input
+            type="text"
+            name="department"
+            onChange={handleInputChange}
+            // value={serviceDetails.department}
+            className="block w-full py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          />
+        </div>
+
+        <div className="mb-2 flex flex-row items-center">
+          <label className="flex flex-none mr-2">employee Id:</label>
+          <input
+            type="text"
+            name="employeeId"
+            onChange={handleInputChange}
+            // value={serviceDetails.employeeId}
+            className="block w-full py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          />
+        </div>
+      </div>
+
+      <div className="mb-2 flex flex-row items-center">
+        <label className="flex flex-none mr-2">designation:</label>
+        <input
+          type="text"
+          name="designation"
+          onChange={handleInputChange}
+          // value={serviceDetails.designation}
+          className="block w-full py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        />
+      </div>
+
+      <div className="mb-2 flex flex-row items-center">
+        <label className="flex flex-none mr-2">reason Of Request:</label>
+        <input
+          type="text"
+          name="reasonOfRequest"
+          onChange={handleInputChange}
+          // value={serviceDetails.reasonOfRequest}
+          className="block w-full py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        />
+      </div>
+
+      <div className="mb-2 flex flex-row items-center">
+        <label className="flex flex-none mr-2">service Details:</label>
+        <input
+          type="text"
+          name="serviceDetails"
+          onChange={handleInputChange}
+          // value={serviceDetails.serviceDetails}
+          className="block w-full py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        />
+      </div>
+
+      <div className="flex justify-between">
+        <button
+          type="button"
+          className="bg-gray-500 text-white p-2 w-32 rounded hover:bg-gray-700"
+        >
+          <Link href="/dashboard">Back</Link>
+        </button>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-700"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default EditServiceRequest;
