b37f66e96723b6139850225b10e4a0de801364f8
injammahin <injammahin5507@gmail.com>
2024-01-23 13:40:12 +0600

    s

--- src/app/[dashboard]/headOfDepartment-dashboard/page.tsx
+++ src/app/[dashboard]/headOfDepartment-dashboard/page.tsx
@@ -210,7 +210,7 @@ const ServiceRequests: React.FC = () => {
                       onClick={() => handleAction(request.id, "cancel")}
                       className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
                     >
-                      cancel
+                      decline
                     </button>
                   </div>
                   <button
