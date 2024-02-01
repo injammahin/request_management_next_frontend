4fe7a1e657e6d335bcb05dddc12a48ee8e65df03
injammahin <injammahin5507@gmail.com>
2024-02-01 21:03:57 +0600

    a

--- src/app/user/service_request/page.tsx
+++ src/app/user/service_request/page.tsx
@@ -59,8 +59,8 @@ const RequestServiceForm: React.FC<RequestServiceFormProps> = ({}) => {
     revokeBy: "",
     execusion: "",
     deviceApprovedBy: "",
-    superadmin: localStorage.getItem("subadmin") || "",
-    subadmin: localStorage.getItem("superadmin") || "",
+    superadmin: localStorage.getItem("superadmin") || "",
+    subadmin: localStorage.getItem("subadmin") || "",
     ciso: localStorage.getItem("ciso") || "",
     head: localStorage.getItem("head") || "",
   });
@@ -462,7 +462,7 @@ const RequestServiceForm: React.FC<RequestServiceFormProps> = ({}) => {
                 name="revokeBy"
                 // onChange={handleInputChange}
                 readOnly
-                value={serviceDetails.superadmin}
+                value={serviceDetails.subadmin}
                 className="block w-full py-2.5 px-0 text-lg underline underline-offset-4 text-gray-900 bg-transparent text-center border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               />
               <label className="font-semibold flex flex-none text-sm mr-2  mx-6 ">
@@ -481,10 +481,10 @@ const RequestServiceForm: React.FC<RequestServiceFormProps> = ({}) => {
                   if (e.target.value === "Head of Irfa") {
                     setServiceDetails({
                       ...serviceDetails,
-                      subadmin: "subadmin",
+                      superadmin: "superadmin",
                     });
                   } else {
-                    setServiceDetails({ ...serviceDetails, subadmin: "" });
+                    setServiceDetails({ ...serviceDetails, superadmin: "" });
                   }
                 }}
                 className="block w-full py-2.5 px-0 text-center text-sm text-gray-900 bg-transparent border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
