bdfd395b96ab66fc5abde1ff6ddf42d54c37b09b
injammahin <injammahin5507@gmail.com>
2024-02-11 16:09:24 +0600

    contract us done

--- src/app/components/navigation/page.tsx
+++ src/app/components/navigation/page.tsx
@@ -219,6 +219,14 @@ const Navbar: React.FC<NavbarProps> = ({ userRole, onMenuToggle }) => {
                       Logout
                     </Link>
                   </li>
+                  <li>
+                    <a
+                      href="/user/contract-us"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Contract Us
+                    </a>
+                  </li>
                 </ul>
               </div>
             )}
